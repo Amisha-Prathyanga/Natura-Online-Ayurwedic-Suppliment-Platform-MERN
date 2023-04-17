155553b6c99c3e5e3633d329e63e25fa4cb32f46
Amisha Prathyanga <spbamisha@gmail.com>
2023-04-17 22:44:43 +0530

    stripe payment gateway almost complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,47 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./components/checkoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51MxnUfIr55hAMMKQuMS7wvYObxnJIi1y1iBbWUtkkHDx4jXFEo98NJDOzjUYEcFJftzUwyd5LeS63w442jBMy7iM00GdnVjReF"
+);
 
 function App() {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:3001/api/create-payment-intent", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <h1>Hello World !!!!?</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello World !!!!?</h1> */}
+      {/* <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements> */}
+
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
     </div>
   );
 }
